a7a323d1aa2e04014505619f3279c083ef85a512
Michael777aa <shoxabdufattoyev485@gmail.com>
2024-05-26 19:20:39 +0900

    fix

---
+++ app.js
@@ -0,0 +1,62 @@
+console.log("Web Serverni boshlash");
+const express = require("express");
+const app = express();
+
+
+
+
+
+// MongoDB connect 
+// const { connectDB } = require('./server').db();
+const db  = require("./server").db();
+const mongodb = require("mongodb");
+// import { db } from "./server.js";
+
+// 1  Kirish code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+// 2 : Session code
+// 3 Views code
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+
+// 4 Routing code
+app.post("/create-item", (req, res) =>{
+    console.log('user entered /create-item');
+    console.log(req.body);
+    const new_reja = req.body.reja;
+    db.collection("plans").insertOne({reja: new_reja}, (err, data) =>{
+        console.log(data.ops);
+       res.json(data.ops[0]);
+    })
+    
+});
+
+app.post("/delete-item",(req, res) => {
+const id = req.body.id;
+db.collection("plans").deleteOne({_id: new mongodb.ObjectId(id)}, function (err, data) {
+    res.json({state: "success"});
+})
+});
+
+app.get("/", function (req, res) {
+    console.log('user entered /');
+    db.collection("plans")
+    .find()
+    .toArray((err, data) => {
+        if(err) {
+            console.log(err);
+            res.end("something went wrong")
+        }else{
+            res.render("reja",{items:data});
+        }
+    })
+    
+  });
+
+
+  module.exports = app;
---
+++ server.js
@@ -0,0 +1,38 @@
+const http = require("http");
+
+const mongodb = require("mongodb");
+
+let db;
+
+
+const connectionString = "mongodb+srv://abdullahjust777:yVh1Gj3lZ9urhccB@michael.vcnsjnq.mongodb.net/Reja";
+mongodb.connect(connectionString,
+{
+    useNewUrlParser: true,
+    useUnifiedTopology:true,
+},
+
+(err, client) => {
+    if(err) console.log("ERROR on connection MongoDB");
+    else{
+        console.log("MongoDB connection succeed");
+        module.exports = client;
+        // db = client.db("Reja");
+        const app = require("./app");
+        const server = http.createServer(app);
+       
+        let PORT = 3000;
+        server.listen(PORT, function () {
+        console.log(
+            `ther server is running successfully on port: ${PORT}, http://localhost:${PORT}`
+             );
+        });
+
+    }
+}
+
+);
+// module.exports = { connectDB };
+// module.exports = db;
+//MERN va JavaScript fullstack kursimizning birinchi CRUD loyihasi
+
