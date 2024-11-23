74a39569ba333eefd2addfad6e33a3f0462dcdd1
Michael777aa <shoxabdufattoyev485@gmail.com>
2024-11-23 22:31:45 +0900

    feat: create Dockerfile to generate images

--- server.js
+++ server.js
@@ -4,35 +4,33 @@ const mongodb = require("mongodb");
 
 let db;
 
-
-const connectionString = "mongodb+srv://abdullahjust777:yVh1Gj3lZ9urhccB@michael.vcnsjnq.mongodb.net/Reja";
-mongodb.connect(connectionString,
-{
+const connectionString =
+  "mongodb+srv://abdullahjust777:yVh1Gj3lZ9urhccB@michael.vcnsjnq.mongodb.net/Reja";
+mongodb.connect(
+  connectionString,
+  {
     useNewUrlParser: true,
-    useUnifiedTopology:true,
-},
+    useUnifiedTopology: true,
+  },
 
-(err, client) => {
-    if(err) console.log("ERROR on connection MongoDB");
-    else{
-        console.log("MongoDB connection succeed");
-        module.exports = client;
-        // db = client.db("Reja");
-        const app = require("./app");
-        const server = http.createServer(app);
-       
-        let PORT = 3000;
-        server.listen(PORT, function () {
+  (err, client) => {
+    if (err) console.log("ERROR on connection MongoDB");
+    else {
+      console.log("MongoDB connection succeed");
+      module.exports = client;
+      db = client.db("Reja");
+      const app = require("./app");
+      const server = http.createServer(app);
+
+      let PORT = 3000;
+      server.listen(PORT, function () {
         console.log(
-            `ther server is running successfully on port: ${PORT}, http://localhost:${PORT}`
-             );
-        }); 
-
-    } 
-}
-
+          `ther server is running successfully on port: ${PORT}, http://localhost:${PORT}`
+        );
+      });
+    }
+  }
 );
 // module.exports = { connectDB };
 // module.exports = db;
 //MERN va JavaScript fullstack kursimizning birinchi CRUD loyihasi
-
